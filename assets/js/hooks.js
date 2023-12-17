6d8e4137243d42afa0d6905514d9d93a9bc8b548
Andy <andy@conspyre.com>
2023-12-17 14:24:01 -0500

    Add layers for bands.

--- assets/js/hooks.js
+++ assets/js/hooks.js
@@ -5,7 +5,56 @@ import utc from '../vendor/dayjs/plugin/utc';
 
 let Hooks = {};
 let map = null;
-let markers = [];
+let layerControl = null;
+let allMarkers = [];
+let layers = [];
+let bands = [
+  '160m',
+  '80m',
+  '60m',
+  '40m',
+  '30m',
+  '20m',
+  '17m',
+  '15m',
+  '12m',
+  '10m',
+  '8m',
+  '6m',
+  '5m',
+  '4m',
+  '2m',
+  '1.25cm',
+  '70cm',
+  '33cm',
+  'vlf',
+  '4000m',
+  '2200m',
+  '600m',
+  '23cm',
+  '2.4Ghz',
+  '3.4Ghz',
+  '5.8Ghz',
+  '10Ghz',
+  '24Ghz',
+  '47Ghz',
+  '76Ghz',
+  'vhf/uhf',
+];
+
+let default_bands = [
+  '160m',
+  '80m',
+  '60m',
+  '40m',
+  '30m',
+  '20m',
+  '17m',
+  '15m',
+  '12m',
+  '10m',
+  '6m',
+];
 
 Hooks.QsoMap = {
   mounted() {
@@ -16,7 +65,18 @@ Hooks.QsoMap = {
     });
   },
   doMap(latitude, longitude) {
-    map = L.map('world-map').setView([latitude, longitude], 3);
+    map = L.map('world-map', {
+      center: [latitude, longitude],
+      zoom: 3,
+    });
+
+    for (let i = 0; i < bands.length; i++) {
+      layers[bands[i]] = L.layerGroup([]);
+      if (default_bands.includes(bands[i])) {
+        map.addLayer(layers[bands[i]]);
+      }
+    }
+
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       minZoom: 4,
       maxZoom: 6,
@@ -24,6 +84,10 @@ Hooks.QsoMap = {
       attribution:
         '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     }).addTo(map);
+
+    layerControl = L.control
+      .layers(null, layers, { position: 'bottomleft' })
+      .addTo(map);
 
     var bounds = map.getBounds(); // Get the current bounds of the map
     map.setMaxBounds(bounds);
@@ -48,20 +112,21 @@ Hooks.QsoMap = {
     var band_colors = {
       '160m': '#16a34a',
       '80m': '#84cc16',
-      '60m': '#f97316',
-      '40m': '#f97316',
+      '60m': '#e5e7eb',
+      '40m': '#164e63',
       '30m': '#3f6212',
-      '20m': '#ef4444',
+      '20m': '#6b7280',
       '17m': '#14b8a6',
       '15m': '#06b6d4',
       '12m': '#6366f1',
       '10m': '#79C99E',
-      '6m': '#97DB4F',
+      '6m': '#881337',
       '2m': '#450a0a',
       '1.25cm': '#1a2e05',
       '70cm': '#083344',
       '33cm': '#1e1b4b',
     };
+
     this.handleEvent('new_spot', (spot) => {
       var band = spot.spot.band;
       var mode = spot.spot.mode;
@@ -69,28 +134,31 @@ Hooks.QsoMap = {
       var grid = spot.spot.recv_grid;
       var lat = spot.spot.lat;
       var lon = spot.spot.lon;
-      var marker = L.circleMarker([lat, lon], {
-        radius: 3,
-        width: 3,
-        color: band_colors[band],
-        fillColor: band_colors[band],
-        fillOpacity: 1,
-        fill: true,
-      }).addTo(map);
-      marker.bindPopup(
-        '<strong>' +
-          sender +
-          '</strong><br/>' +
-          mode +
-          '/' +
-          band +
-          '<br/> Heard: ' +
-          grid,
-      );
-      markers.push(marker);
-      if (markers.length >= 1000) {
-        remove = markers.shift();
-        map.removeLayer(remove);
+      if (bands.includes(band)) {
+        var marker = L.circleMarker([lat, lon], {
+          radius: 3,
+          width: 3,
+          color: band_colors[band],
+          fillColor: band_colors[band],
+          fillOpacity: 1,
+          fill: true,
+        }).addTo(layers[band]);
+        marker.bindPopup(
+          '<strong>' +
+            sender +
+            '</strong><br/>' +
+            mode +
+            '/' +
+            band +
+            '<br/> Heard: ' +
+            grid,
+        );
+
+        allMarkers.push({ layer: layers[band], marker: marker });
+        if (allMarkers.length > 1000) {
+          var removeMe = allMarkers.shift();
+          removeMe.layer.removeLayer(removeMe.marker);
+        }
       }
     });
   },
