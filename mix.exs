746e9672740a3f41b5dfca6dc5f377ad6e88edd0
Andy <andy@conspyre.com>
2023-12-17 14:47:03 -0500

    Update workflow to have asset building.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Hamchron.MixProject do
   def project do
     [
       app: :hamchron,
-      version: "0.1.0",
+      version: "0.3.0",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
